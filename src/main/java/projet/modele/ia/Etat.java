9e70980fde391c4672edb045fc0d4ee6f23dd9c6
Eva PURSON <s.p.y.p.o.w.e.r.005@gmail.com>
2023-11-17 17:53:10 +0100

    Ajout distance de Manhattan

--- src/main/java/projet/modele/ia/Etat.java
+++ src/main/java/projet/modele/ia/Etat.java
@@ -4,18 +4,25 @@ import projet.modele.game.Grille;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.lang.Math;
 
-public class Etat implements Cloneable{
+import static java.lang.Math.ceil;
+import static java.lang.Math.ceilDiv;
+
+public class Etat implements Cloneable {
     private int[][] etatGrille;
     private int xVide, yVide;
     private int longueur;
     private ArrayList<String> mouvements;
     private int profondeur;
 
+    private int heuristique;
 
-    private Etat(){}
 
-    public Etat(Grille g){
+    private Etat() {
+    }
+
+    public Etat(Grille g) {
         this.profondeur = 0;
         this.mouvements = new ArrayList<>();
         this.longueur = g.getLongueur();
@@ -23,18 +30,18 @@ public class Etat implements Cloneable{
         this.xVide = g.retournerCaseVide().getX();
         this.yVide = g.retournerCaseVide().getY();
 
-        for(int i = 0;i<g.getLongueur();i++){
-            for(int j = 0;j<g.getLongueur();j++){
+        for (int i = 0; i < g.getLongueur(); i++) {
+            for (int j = 0; j < g.getLongueur(); j++) {
                 this.etatGrille[i][j] = g.transformerGrilleArray2D()[i][j].getIndice();
             }
         }
     }
 
-    public int getProfondeur(){
+    public int getProfondeur() {
         return this.profondeur;
     }
 
-    protected Etat clone() throws CloneNotSupportedException{
+    protected Etat clone() throws CloneNotSupportedException {
         Etat e2 = (Etat) super.clone();
 
         Etat e3 = new Etat();
@@ -45,84 +52,111 @@ public class Etat implements Cloneable{
         e3.profondeur = e2.profondeur;
         e3.mouvements = new ArrayList<>(e2.mouvements);
         e3.etatGrille = new int[e3.longueur][e3.longueur];
-        for(int i = 0;i<e3.longueur;i++){
+        for (int i = 0; i < e3.longueur; i++) {
             System.arraycopy(e2.etatGrille[i], 0, e3.etatGrille[i], 0, e3.longueur);
         }
 
         return e3;
     }
 
-    public int[][] getEtatGrille(){
+    public int[][] getEtatGrille() {
         return this.etatGrille;
     }
 
-    public ArrayList<String> getMouvements(){
+    public ArrayList<String> getMouvements() {
         return this.mouvements;
+    }
+
+    private int[] calculerPosVictoire(int val) {
+// valeur de la longueur - 1 et if
+        int[] result = new int[2];
+        result[0] = (int)  ceil(val / this.longueur) -1;
+        int temp = val % this.longueur - 1;
+        if(temp == -1) {
+            result[1] = this.longueur - 1;
+        }else{
+            result[1] = temp;
+        }
+        return result;
+    }
+
+    public void ManhattanDistance() {
+
+        int result = 0;
+
+        for(int i = 0; i < this.longueur - 1; i++) {
+            for(int j = 0; j < this.longueur - 1; j++) {
+                int[] coordonneesVictoire = this.calculerPosVictoire(this.etatGrille[i][j]);
+                result += Math.abs (coordonneesVictoire[0]-i) + Math.abs (coordonneesVictoire[1]-j);
+            }
+        }
+        this.heuristique = result;
+
     }
 
     public Etat simulerDeplacement(IA.DEPLACEMENT d) throws CloneNotSupportedException {
         Etat e = null;
-        switch (d){
+        switch (d) {
             case haut:
-                if(this.xVide+1<this.longueur) {
+                if (this.xVide + 1 < this.longueur) {
                     e = this.clone();
                     int tmp = e.etatGrille[e.xVide][e.yVide];
-                    e.etatGrille[e.xVide][e.yVide] = e.etatGrille[e.xVide+1][e.yVide];
-                    e.etatGrille[e.xVide+1][e.yVide] = tmp;
+                    e.etatGrille[e.xVide][e.yVide] = e.etatGrille[e.xVide + 1][e.yVide];
+                    e.etatGrille[e.xVide + 1][e.yVide] = tmp;
                     e.xVide++;
                     e.mouvements.add("haut");
-                    e.profondeur ++;
+                    e.profondeur++;
                 }
                 break;
             case bas:
-                if(this.xVide-1>=0) {
+                if (this.xVide - 1 >= 0) {
                     e = this.clone();
                     int tmp = e.etatGrille[e.xVide][e.yVide];
-                    e.etatGrille[e.xVide][e.yVide] = e.etatGrille[e.xVide-1][e.yVide];
-                    e.etatGrille[e.xVide-1][e.yVide] = tmp;
+                    e.etatGrille[e.xVide][e.yVide] = e.etatGrille[e.xVide - 1][e.yVide];
+                    e.etatGrille[e.xVide - 1][e.yVide] = tmp;
                     e.xVide--;
                     e.mouvements.add("bas");
-                    e.profondeur ++;
+                    e.profondeur++;
                 }
                 break;
             case gauche:
-                if(this.yVide+1<this.longueur) {
+                if (this.yVide + 1 < this.longueur) {
                     e = this.clone();
                     int tmp = e.etatGrille[e.xVide][e.yVide];
-                    e.etatGrille[e.xVide][e.yVide] = e.etatGrille[e.xVide][e.yVide+1];
-                    e.etatGrille[e.xVide][e.yVide+1] = tmp;
+                    e.etatGrille[e.xVide][e.yVide] = e.etatGrille[e.xVide][e.yVide + 1];
+                    e.etatGrille[e.xVide][e.yVide + 1] = tmp;
                     e.yVide++;
                     e.mouvements.add("gauche");
-                    e.profondeur ++;
+                    e.profondeur++;
                 }
                 break;
             case droite:
-                if(this.yVide-1>=0) {
+                if (this.yVide - 1 >= 0) {
                     e = this.clone();
                     int tmp = e.etatGrille[e.xVide][e.yVide];
                     e.etatGrille[e.xVide][e.yVide] = e.etatGrille[e.xVide][e.yVide - 1];
                     e.etatGrille[e.xVide][e.yVide - 1] = tmp;
                     e.yVide--;
                     e.mouvements.add("droite");
-                    e.profondeur ++;
+                    e.profondeur++;
                 }
                 break;
         }
         return e;
     }
 
-    public boolean estComparable(int[][] e){
-        if(e.length == this.etatGrille.length){
+    public boolean estComparable(int[][] e) {
+        if (e.length == this.etatGrille.length) {
             return e[0].length == this.etatGrille[0].length;
         }
         return false;
     }
 
     @Override
-    public int hashCode(){
+    public int hashCode() {
         StringBuilder sb = new StringBuilder();
-        for(int[] row : this.etatGrille){
-            for(int i : row){
+        for (int[] row : this.etatGrille) {
+            for (int i : row) {
                 sb.append(i);
             }
         }
@@ -142,10 +176,10 @@ public class Etat implements Cloneable{
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         String s = "";
-        for(int i=0;i<this.longueur;i++){
-            s = s+Arrays.toString(this.etatGrille[i])+"\n";
+        for (int i = 0; i < this.longueur; i++) {
+            s = s + Arrays.toString(this.etatGrille[i]) + "\n";
         }
         return s;
     }
